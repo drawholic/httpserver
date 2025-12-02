8b8a56960d4781a722db4f14f17eb3b0d2668b06
vladick <mamrokhavlad@gmail.com>
2025-12-02 20:34:48 +0200

    server declaration. update network headers, fields with socket, addr, port. added loop method

--- include/server.hpp
+++ include/server.hpp
@@ -2,11 +2,23 @@
 #define SERVER_HPP
 
 
+#include "sys/socket.h"
+#include <netinet/in.h>
+#include <unistd.h>
+
+
+
 class Server
 {
+	int socket;
+	sockaddr_in addr;
+	uint16_t port;
+
+	void setup();
 
 public:
 	Server();
+	void loop();
 };
 
 #endif
