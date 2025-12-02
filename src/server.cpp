c1d144b86b00c77467f043cb995fce8a781027b5
vladick <mamrokhavlad@gmail.com>
2025-12-02 20:35:15 +0200

    setup method. filled with socket creation, binding, setsockopt

--- src/server.cpp
+++ src/server.cpp
@@ -4,4 +4,40 @@
 Server::Server()
 {
 
+
+
+
+};
+
+
+void Server::setup()
+{
+	int opt = 1;
+	int addrlen = sizeof(sockaddr);
+
+
+	socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket == 0)
+	{
+		perror("socket failure");
+		exit(EXIT_FAILURE);
+	}
+
+	if(setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+	{
+		perror("setsockopt SO_REUSEADDR failed");
+		close(socket);
+		exit(EXIT_FAILURE);
+	};
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = port = htons(8080);
+
+	if(bind(socket, (struct sockaddr*)&addr, addrlen) < 0)
+	{
+		perror("bind failed");
+		close(socket);
+		exit(EXIT_FAILURE);
+	}
 };
