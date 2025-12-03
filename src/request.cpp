7b3a4354d135899395ea873ae4598a14ffd22646
vladick <mamrokhavlad@gmail.com>
2025-12-03 16:13:17 +0200

    set_request str implementation

--- src/request.cpp
+++ src/request.cpp
@@ -27,5 +27,9 @@ char* Request::get_startline()
 
 void Request::set_request_str(char* req)
 {
+	if(request_str)
+		delete[] request_str;
+	request_str = new char[strlen(req)];
+	strncpy(request_str, req, strlen(request_str));
 
 };
