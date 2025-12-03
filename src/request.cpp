c99a0ab17acd7d8a859a30673824683e08b0d9d6
vladick <mamrokhavlad@gmail.com>
2025-12-03 16:45:12 +0200

    set_request_str refactor. added nullifying the request_str, proper check for null pointer. get_startline implementation

--- src/request.cpp
+++ src/request.cpp
@@ -20,16 +20,33 @@ char* Request::get_body()
 
 };
 
+#include <iostream>
 char* Request::get_startline()
 {
+	if(!request_str) return 0;
 
+	char* newline = strchr(request_str, '\n');
+
+	if(!newline) return 0;
+
+	size_t len = newline - request_str;
+
+	if(len > 0 && request_str[len-1] == '\r') len--;	
+
+	char* startline = new char[len + 1];
+	memcpy(startline, request_str, len);
+	startline[len] = 0;
+
+	return startline;
 };
 
-void Request::set_request_str(char* req)
+void Request::set_request_str(const char* req)
 {
-	if(request_str)
+	if(!request_str)
 		delete[] request_str;
-	request_str = new char[strlen(req)];
-	strncpy(request_str, req, strlen(request_str));
 
-};
+	request_str = new char[strlen(req)];
+
+	strncpy(request_str, req, strlen(req));
+	request_str[strlen(req)] = 0;
+};	
