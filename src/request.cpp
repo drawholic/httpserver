389e80d3e76725e21ee6179efb9144dd646effa6
vladick <mamrokhavlad@gmail.com>
2025-12-03 16:07:32 +0200

    empty methods definitions

--- src/request.cpp
+++ src/request.cpp
@@ -4,3 +4,23 @@ Request::Request()
 {
 
 };
+
+char* Request::get_method()
+{
+
+};
+
+char* Request::get_headers()
+{
+
+};
+
+char* Request::get_body()
+{
+
+};
+
+char* Request::get_startline()
+{
+
+};
