59bfda89c929db9e48c0cca42594da2486b26746
vladick <mamrokhavlad@gmail.com>
2025-12-05 18:31:35 +0200

    response class fields added. included request.hpp

--- include/response.hpp
+++ include/response.hpp
@@ -1,11 +1,23 @@
 #ifndef RESPONSE_HPP
 #define RESPONSE_HPP
 
+#include "request.hpp"
+
 class Response
 {
 
+	char* status_line;
+
+	char* status_code;
+	char* reason_phrase;
+	char* headers;
+	char* body;
+
 public:
 	Response();
+	void set_request(Request&);
+	char* get_response_str();
+
 };
 
 #endif
